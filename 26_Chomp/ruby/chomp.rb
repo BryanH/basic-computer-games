94f55bf640bb21880fbcc30caea61b36f5b52f14
Bryan Hanks, PMP CSP-SM <bryan@master-developer.com>
2024-10-08 19:44:37 -0500

    Intro

--- 26_Chomp/ruby/chomp.rb
+++ 26_Chomp/ruby/chomp.rb
@@ -7,4 +7,6 @@
 #
 
 puts("\tCHOMP")
-puts('Creative Computing Morristown, New Jersey')
+puts("Creative Computing Morristown, New Jersey\n\n\n\n")
+puts('This is the game of Chomp (scientific American, Jan 1973)')
+puts('Want the rules (1 = yes, 0 = no)?')
