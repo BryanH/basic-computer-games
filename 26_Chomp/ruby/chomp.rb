75cd0d41789bc17f59848f06f73c98cc1c48b89f
Bryan Hanks, PMP CSP-SM <bryan@master-developer.com>
2024-10-08 19:48:41 -0500

    Rubocop

--- 26_Chomp/ruby/chomp.rb
+++ 26_Chomp/ruby/chomp.rb
@@ -5,8 +5,10 @@
 #
 # Converted from BASIC to Ruby by BryanH
 #
-
-puts("\tCHOMP")
-puts("Creative Computing Morristown, New Jersey\n\n\n\n")
-puts('This is the game of Chomp (scientific American, Jan 1973)')
-puts('Want the rules (1 = yes, 0 = no)?')
+puts(<<~HEREDOC
+  \tChomp
+  Creative Computing Morristown, New Jersey\n\n\n
+  This is the game of Chomp (scientific American, Jan 1973)
+  Want the rules (1 = yes, 0 = no)?
+HEREDOC
+    )
